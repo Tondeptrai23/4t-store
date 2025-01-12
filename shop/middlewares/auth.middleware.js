8a158d15a467f97ea82068862339b1ece1678eb1
Tondeptrai23 <22120375@student.hcmus.edu.vn>
2025-01-12 17:22:22 +0700

    Add isAdmin middleware and error page

--- shop/middlewares/auth.middleware.js
+++ shop/middlewares/auth.middleware.js
@@ -1,13 +1,30 @@
 import { DeserializeError } from "../utils/errors.js";
 
 export function deserializeHandler(error, request, response, next) {
-	if (error && error instanceof DeserializeError) {
-		request.logout((error) => {
-			if (error) { throw new Error(error); }
-			response.redirect("/");
-		});
-	}
-	else {
-		next();
-	}
+    if (error && error instanceof DeserializeError) {
+        request.logout((error) => {
+            if (error) {
+                throw new Error(error);
+            }
+            response.redirect("/");
+        });
+    } else {
+        next();
+    }
+}
+
+export function isAdmin(request, response, next) {
+    if (request.isAuthenticated() && request.user.role === "admin") {
+        console.log("Admin: ", request.user);
+        next();
+    } else {
+        response.status(403).render("index", {
+            body: "error",
+            isLoggedIn: request.user ? true : false,
+            status: 403,
+            message: "Bạn không có quyền truy cập trang này",
+            description:
+                "Vui lòng đăng nhập với tài khoản admin để truy cập trang này",
+        });
+    }
 }
--- shop/routes/admin.route.js
+++ shop/routes/admin.route.js
@@ -1,10 +1,12 @@
 import express from "express";
 import expressLayouts from "express-ejs-layouts";
-import adminProductController from "../controllers/adminProduct.controller.js";
 import adminCategoryController from "../controllers/adminCategory.controller.js";
+import adminProductController from "../controllers/adminProduct.controller.js";
+import { isAdmin } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 
+router.use(isAdmin);
 router.use(expressLayouts);
 
 router.get("/products", adminProductController.listProducts);
@@ -15,12 +17,15 @@ router.put("/products/:id", adminProductController.updateProduct);
 router.delete("/products/:id", adminProductController.deleteProduct);
 router.post("/products/bulk-delete", adminProductController.bulkDeleteProducts);
 
-router.get('/categories', adminCategoryController.listCategories);
+router.get("/categories", adminCategoryController.listCategories);
 router.get("/categories/create", adminCategoryController.showCreateForm);
-router.post('/categories/create', adminCategoryController.createCategory);
+router.post("/categories/create", adminCategoryController.createCategory);
 router.get("/categories/edit/:id", adminCategoryController.showEditForm);
 router.put("/categories/:id", adminCategoryController.updateCategory);
 router.delete("/categories/:id", adminCategoryController.deleteCategory);
-router.post("/categories/bulk-delete", adminCategoryController.bulkDeleteCategories);
+router.post(
+    "/categories/bulk-delete",
+    adminCategoryController.bulkDeleteCategories
+);
 
 export default router;
